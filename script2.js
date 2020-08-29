2a370a45f05f6be81669fa24195958f7f97fa89a
AISWARYAK99 <66054986+AISWARYAK99@users.noreply.github.com>
2020-08-29 21:01:28 +0530

    add link to riddle 4

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 var pass;
 pass = prompt("please enter the answer");
 if (pass == "D") {
-    document.location.href = "";
+    document.location.href = "https://krishnaprasad003.github.io/";
 } else {
     document.location.href="error2.html";
 }
